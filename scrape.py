f778bd07e480612e0ded6b2d3c46eff15d169e36
Ștefan Tănăsescu <stefantanasescu91@gmail.com>
2023-02-20 15:30:28 +0200

    fixed text sanitisation and added logger

--- scrape.py
+++ scrape.py
@@ -5,7 +5,7 @@ import time,utils,string
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from utils import config
+from utils import config, sanitize_text
  
  
 def scrape(post_url):
@@ -100,23 +100,22 @@ from prawcore.exceptions import ResponseException
 import spacy
 
 def is_post_valid(post, post_type):
-    if not exists("log.json"):
-        with open("log.json", "w+") as f:
+    if not exists("rsc/log.json"):
+        with open("rsc/log.json", "w+") as f:
             json.dump([], f)
     with open(
-        "log.json", "r", encoding="utf-8"
+        "rsc/log.json", "r", encoding="utf-8"
     ) as done_vids_raw:
         done_videos = json.load(done_vids_raw)
     
-    done_ids=[]
-    for i in done_videos:
-        done_ids.append(i['id'])
     
-    if str(post) in done_ids or \
+    if str(post) in done_videos.keys() or \
         post.over_18 or \
         post.stickied or \
-        post.num_comments < 30 or \
-        post_type == "story" and not post.is_self:
+        (post_type== "comments" and post.num_comments < 30) or \
+        (post_type == "story" and post.selftext == "") or \
+        (post_type == "story" and post.selftext in ["[removed]", "[deleted]"]) or \
+        (post_type == "story" and len(post.selftext) >6000):
         print("Invalid post: " + str(post))
         return False
         
@@ -126,7 +125,7 @@ def is_comment_valid(comment):
     if isinstance(comment, MoreComments) or \
         comment.body in ["[removed]", "[deleted]"] or \
         comment.stickied or \
-        len(comment.body) > 8000 or \
+        len(comment.body) > 300 or \
         len(comment.body) < 3 or \
         comment.author is None:
             return False
@@ -138,20 +137,22 @@ def is_comment_valid(comment):
 def scrape_post(sub, post_type):
     # returns data={}, where data is an object of the form:
     # post_type: 'story' or 'comments'
+    # sub: subreddit name
     # id: id of the post
     # url: url of the post
     # title: post title
     # text: sentences of the post in a list
     # ^ empty if post_type isn't 'story'
     # comments: list of objects of the form {
-        #comment_id
-        #comment_url
-        #comment_text
+        #id
+        #url
+        #text
     #}
     # ^ empty if post_type isn't 'comments'
     
     data = {}
     data["post_type"] = post_type
+    data["sub"] = sub
     
     # Creating reddit instance
     try:
@@ -187,7 +188,7 @@ def scrape_post(sub, post_type):
     print(f"Post url is : {post_url}")
     data["id"] = post.id
     data["url"] = post_url
-    data["title"] = post.title
+    data["title"] = sanitize_text(post.title)
     data["text"] = []
     data["comments"] = []
     
@@ -201,12 +202,14 @@ def scrape_post(sub, post_type):
             print("The spacy model can't load. You need to install it with \npython -m spacy download en")
             exit(1)
         doc = nlp(text)
-        data["post_text"] = [sent.text for sent in doc.sents]
+        data["text"] = [sent.text for sent in doc.sents if sent.text not in ["x200B", "", " "]]
         
     elif post_type == "comments":
         # If comments, the comments get saved into the [comments] list and the [post_text] is empty
-        for comment in post.comments[:config["no_of_comments"]]:
-            if is_comment_valid(comment): 
+        needed = config["no_of_comments"]
+        for comment in post.comments:
+            if is_comment_valid(comment):
+                needed-=1
                 comment.body = sanitize_text(comment.body)
                 data["comments"].append(
                     {
@@ -215,6 +218,8 @@ def scrape_post(sub, post_type):
                         "id": comment.id,
                     }
                 )
+            if needed == 0:
+                break
                 
     else:
         print("Invalid post type")
--- utils.py
+++ utils.py
@@ -1,14 +1,11 @@
+import json
 import yaml
 import undetected_chromedriver as uc
+import re
+import os
 
 # Load and validate config
 config = yaml.safe_load(open('config.yaml').read())
-
-if not config['tiktok_cookies']:
-    raise Exception('Missing TikTok Cookies')
-
-if not config['reddit_cookies']:
-    raise Exception('Missing Reddit Cookies')
 
 def create_bot(headless=False):
     options = uc.ChromeOptions()
@@ -22,3 +19,41 @@ def create_bot(headless=False):
     bot.set_page_load_timeout(25)
     bot.set_window_size(1920, 1080)
     return bot
+
+def load_orders(path):
+    orders = []
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            orders.append(line.split())
+    return orders
+
+def sanitize_text(text):
+    # remove any urls from the text
+    regex_urls = r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*"
+    result = re.sub(regex_urls, " ", text)
+
+    # remove `^_~@!&;#:-%“”‘"%*/{}[]()\|<>?=+`
+    regex_expr = r"\s['|’]|['|’]\s|[\^_~@!&;#:\-%—“”‘\"%\*/{}\[\]\(\)\\|<>=+]"
+    result = re.sub(regex_expr, " ", result)
+    result = result.replace("+", "plus").replace("&", "and")
+ 
+    # remove extra whitespace
+    return " ".join(result.split())
+
+def log(post):
+    if not os.path.exists('rsc/log.json'):
+        with open('rsc/log.json', 'w+') as f:
+            f.write('[]')
+
+    with open('rsc/log.json', 'r') as f:
+        data = json.load(f)
+
+    data[post["id"]] = post["url"]
+
+    with open('rsc/log.json', 'w') as f:
+        json.dump(data, f)
+
+def clean_temp():
+    for file in os.listdir('temp'):
+        os.remove(f'temp/{file}')
